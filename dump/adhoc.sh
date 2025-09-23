25fa9682bc388f8eb12070fbed861f18409d5c70
Cory Latschkowski <koree@redhat.com>
2025-09-23 10:37:26 -0600

    add: script

--- dump/adhoc.sh
+++ dump/adhoc.sh
@@ -7,3 +7,22 @@ sudo chmod +x /usr/local/bin/*
 
 rsync -avP scratch/bin/{mirror-registry,*.tar,oc,openshift*,kube*} highside:/mnt/high-side-data/
 
+ssh highside /mnt/high-side-data/mirror-registry install --initPassword discopass
+
+ssh highside sudo cp -v $HOME/quay-install/quay-rootCA/rootCA.pem /etc/pki/ca-trust/source/anchors/
+ssh highside sudo update-ca-trust
+
+scp highside $HOME/quay-install/quay-rootCA/rootCA.pem /tmp/
+sudo cp /tmp/rootCA.pem /etc/pki/ca-trust/source/anchors/
+sudo update-ca-trust
+
+cp $HOME/pull-secret-example.json $XDG_RUNTIME_DIR/containers/auth.json
+REGISTRY=$(ssh highside hostname):8443
+
+podman login -u init -p discopass ${REGISTRY}
+
+oc-mirror 
+  -c scratch/isc-combo.yaml \
+  --workspace file:///${PWD}/scratch/oc-mirror \
+  docker://"${REGISTRY}" \
+  --v2
